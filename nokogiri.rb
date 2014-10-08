e9c728db7ae561c1d3890bb75731d334b51747c1
magic2k <magic2ktech@gmail.com>
2014-10-09 02:01:43 +0400

    Nokogiri test

--- nokogiri.rb
+++ nokogiri.rb
@@ -1,17 +1,32 @@
+#moto.auto.ru parser
+
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML(open('http://moto.auto.ru/motorcycle/used/yamaha/xv-1700-warrior/?currency_key=&price_usd%5B1%5D=&price_usd%5B2%5D=&year%5B1%5D=0&year%5B2%5D=0&client_id=0&region_id=89&stime=0&available_key='))
+# doc = Nokogiri::HTML(open('http://moto.auto.ru/motorcycle/used/yamaha/xv-1700-warrior/?currency_key=&price_usd%5B1%5D=&price_usd%5B2%5D=&year%5B1%5D=0&year%5B2%5D=0&client_id=0&region_id=89&stime=0&available_key='))
+doc = Nokogiri::HTML(open('http://moto.auto.ru/motorcycle/used/honda/bros/')) do |options|
+	options.strict.noblanks.nonet
+end
+
+offer_links = []
 
 doc.css('table.list>tr').each do |l|
-	l.each do |p|
-		puts p
+  #price in the first <nobr>
+	l.css('nobr').each { |p| puts p.content; break;}
+
+  #link to offer and bike name in the same class string
+	l.css('.offer-list').each do |link|
+		puts link.content
+		puts link['href']
+		offer_links << link['href']
 	end
-	# puts l.css['.offer-list'].class
-	puts '_______________________________--'	
-	# puts l['href']	
-	# offer = Nokogiri::HTML(open(l['href']))
-	# offer.css('.cost').each do |c|
-	# 	puts c.content
-	# end
+	puts '____________next >___________________--'
+end
+
+#inside the first offer...
+
+offer = Nokogiri::HTML(open(offer_links[0]))
+puts offer.css('#card-year').each {|y| puts y.content; break }
+offer.css('.cost').each do |c|
+	puts c.content
 end
